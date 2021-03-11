0a290af6cc2f8fd0c7d15eab6fb9be13fe6c9e35
Vladimir Sapronov <vladimir.sapronov@gmail.com>
2021-03-10 23:48:55 -0500

    Removed models from spec

--- target/scala-2.13/src_managed/main/spec/Models.scala
+++ target/scala-2.13/src_managed/main/spec/Models.scala
@@ -13,15 +13,6 @@ case class Message(
   stringField: String
 )
 
-case class Parent(
-  field: String,
-  nested: Nested
-)
-
-case class Nested(
-  field: String
-)
-
 sealed abstract class Choice(val value: String) extends StringEnumEntry
 
 case object Choice extends StringEnum[Choice] with StringCirceEnum[Choice] {
@@ -31,74 +22,6 @@ case object Choice extends StringEnum[Choice] with StringCirceEnum[Choice] {
   val values = findValues
 }
 
-case class EnumFields(
-  enumField: Choice
-)
-
-case class NumericFields(
-  intField: Int,
-  longField: Long,
-  floatField: Float,
-  doubleField: Double,
-  decimalField: BigDecimal
-)
-
-case class NonNumericFields(
-  booleanField: Boolean,
-  stringField: String,
-  uuidField: java.util.UUID,
-  dateField: java.time.LocalDate,
-  datetimeField: java.time.LocalDateTime
-)
-
-case class ArrayFields(
-  intArrayField: List[Int],
-  stringArrayField: List[String]
-)
-
-case class MapFields(
-  intMapField: Map[String, Int],
-  stringMapField: Map[String, String]
-)
-
-case class OptionalFields(
-  intOptionField: Option[Int],
-  stringOptionField: Option[String]
-)
-
-case class RawJsonField(
-  jsonField: io.circe.Json
-)
-
-case class OrderCreated(
-  id: java.util.UUID,
-  sku: String,
-  quantity: Int
-)
-
-case class OrderChanged(
-  id: java.util.UUID,
-  quantity: Int
-)
-
-case class OrderCanceled(
-  id: java.util.UUID
-)
-
-sealed trait OrderEvent
-
-object OrderEvent {
-  case class Created(data: testservice.client.OrderCreated) extends OrderEvent
-  case class Changed(data: testservice.client.OrderChanged) extends OrderEvent
-  case class Canceled(data: testservice.client.OrderCanceled) extends OrderEvent
-}
-
 object json extends AutoDerivation {
   implicit val auto = Configuration.default.withSnakeCaseMemberNames.withSnakeCaseConstructorNames.withDefaults
-  implicit val encoderOrderEventCreated: Encoder[OrderEvent.Created] = deriveUnwrappedEncoder
-  implicit val decoderOrderEventCreated: Decoder[OrderEvent.Created] = deriveUnwrappedDecoder
-  implicit val encoderOrderEventChanged: Encoder[OrderEvent.Changed] = deriveUnwrappedEncoder
-  implicit val decoderOrderEventChanged: Decoder[OrderEvent.Changed] = deriveUnwrappedDecoder
-  implicit val encoderOrderEventCanceled: Encoder[OrderEvent.Canceled] = deriveUnwrappedEncoder
-  implicit val decoderOrderEventCanceled: Decoder[OrderEvent.Canceled] = deriveUnwrappedDecoder
 }
